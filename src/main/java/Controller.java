60ad81440ee594c10060277b427792a77b24b979
Edvard Krainiy <ekrayniy@inbox.ru>
2021-08-13 14:54:42 +0300

    Added primitive UI.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,37 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize() {
+        assert login_field != null : "fx:id=\"login_field\" was not injected: check your FXML file 'application.fxml'.";
+        assert signInButton != null : "fx:id=\"signInButton\" was not injected: check your FXML file 'application.fxml'.";
+        assert password_field != null : "fx:id=\"password_field\" was not injected: check your FXML file 'application.fxml'.";
+        assert signUpButton != null : "fx:id=\"signUpButton\" was not injected: check your FXML file 'application.fxml'.";
+
+        System.out.println("Works!");
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,23 @@
-import dao.AccountDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Person;
-import service.PersonService;
-
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        String fxmlFile = "/fxml/sample.fxml";
+        String fxmlFile = "/fxml/application.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
         primaryStage.setTitle("BankApplication");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        //launch(args);
-        System.out.println("Hibernate tutorial");
+        launch(args);
 
-        PersonService personService = new PersonService();
     }
 }
