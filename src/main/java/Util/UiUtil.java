1afe1f9bca00bf062cf12c15ac4630e625dd5f6e
Edvard Krainiy <ekrayniy@inbox.ru>
2021-08-23 12:50:25 +0300

    Minor changes on UI design. Now Authorization system completely works.

--- src/main/java/Util/UiUtil.java
+++ src/main/java/Util/UiUtil.java
@@ -1,5 +1,6 @@
 package Util;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -9,12 +10,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class UiUtil {
-    public static void goToNextWindow(String FXMLPath, javafx.event.ActionEvent event) throws IOException {
+    public static void goToNextWindow(String FXMLPath, ActionEvent event) throws IOException {
 
         Parent blah = FXMLLoader.load(UiUtil.class.getResource(FXMLPath));
         Scene scene = new Scene(blah);
         Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         appStage.setScene(scene);
         appStage.show();
+    }
+
+    public static void closeWindow(ActionEvent event){
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
--- src/main/java/controller/SignInWindowController.java
+++ src/main/java/controller/SignInWindowController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import service.AccountService;
 
 public class SignInWindowController {
 
@@ -20,29 +22,39 @@ public class SignInWindowController {
     private URL location;
 
     @FXML
-    private TextField login_field;
+    private TextField signInLoginField;
 
     @FXML
     private Button signInButton;
 
     @FXML
-    private PasswordField password_field;
+    private PasswordField signInPasswordField;
+
+    @FXML
+    private Text signInErrorText;
 
     @FXML
     private Button signUpButton;
 
     @FXML
-    void initialize() {
-        assert login_field != null : "fx:id=\"login_field\" was not injected: check your FXML file 'signInWindow.fxml'.";
-        assert signInButton != null : "fx:id=\"signInButton\" was not injected: check your FXML file 'signInWindow.fxml'.";
-        assert password_field != null : "fx:id=\"password_field\" was not injected: check your FXML file 'signInWindow.fxml'.";
-        assert signUpButton != null : "fx:id=\"signUpButton\" was not injected: check your FXML file 'signInWindow.fxml'.";
+    private Button exitButton;
 
-        openSignUpWindow();
+    @FXML
+    void initialize() {
+        assert signInLoginField != null : "fx:id=\"signInLoginField\" was not injected: check your FXML file 'signInWindow.fxml'.";
+        assert signInButton != null : "fx:id=\"signInButton\" was not injected: check your FXML file 'signInWindow.fxml'.";
+        assert signInPasswordField != null : "fx:id=\"signInPasswordField\" was not injected: check your FXML file 'signInWindow.fxml'.";
+        assert signInErrorText != null : "fx:id=\"signInErrorText\" was not injected: check your FXML file 'signInWindow.fxml'.";
+        assert signUpButton != null : "fx:id=\"signUpButton\" was not injected: check your FXML file 'signInWindow.fxml'.";
+        assert exitButton != null : "fx:id=\"exitButton\" was not injected: check your FXML file 'signInWindow.fxml'.";
+
+        initializeSignUpButton();
+        initializeSignInButton();
+        initializeExitButton();
 
     }
 
-    private void openSignUpWindow(){
+    private void initializeSignUpButton(){
         signUpButton.setOnAction(event -> {
             try {
                 UiUtil.goToNextWindow(WindowPath.SIGN_UP_WINDOW, event);
@@ -50,6 +62,36 @@ public class SignInWindowController {
                 e.printStackTrace();
             }
         });
+    }
+
+    private void initializeSignInButton(){
+        signInButton.setOnAction(event -> {
+            String loginText = signInLoginField.getText();
+            String passwordText = signInPasswordField.getText();
+
+            boolean isLoginExisting = AccountService.findByLogin(loginText) != null;
+
+            if(!loginText.equals("") && !passwordText.equals("")){
+
+                String accountPassword = AccountService.findByLogin(loginText).getPassword();
+
+                if(isLoginExisting && accountPassword.equals(passwordText)){
+                    System.out.println("Hello!");
+                }
+                else{
+                    signInErrorText.setVisible(true);
+                    signInErrorText.setText("Логин и/или пароль введены неверно!");
+                }
+            }
+            else {
+                signInErrorText.setVisible(true);
+                signInErrorText.setText("Введите логин и пароль!");
+            }
+        });
+    }
+
+    private void initializeExitButton(){
+        exitButton.setOnAction(UiUtil::closeWindow);
     }
 }
 
